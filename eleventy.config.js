6ffb8c4cff6aaa2250564155145a87b45c31af41
jskinne3 <jno.skinner@gmail.com>
2025-06-09 14:59:46 -0700

    Add image to right side of main text

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,9 @@
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("assets/img/*.png");
+  eleventyConfig.addPassthroughCopy("assets/img/*.jpg");
   eleventyConfig.addPassthroughCopy("assets/css/*.css");
-  eleventyConfig.addPassthroughCopy("assets/vid/*.webm")
-
+  eleventyConfig.addPassthroughCopy("assets/vid/*.webm");
+  
   return {};
 };
